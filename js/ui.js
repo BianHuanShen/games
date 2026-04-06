dbdccb09c165d06f54b11b4dfca5366053a0fc01
BianHuanShen <bianhuanshen.variets@gmail.com>
2026-04-06 15:57:39 -0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -28,9 +28,12 @@ function actualizarUI() {
     `;
 
     // 🔹 Mostrar u ocultar botones de items raros
+    curarBtn.style.display = jugador.inventario.pocion > 0 ? "block" : "none";
     usarCristalBtn.style.display = jugador.inventario.cristal > 0 ? "block" : "none";
     usarOrbeBtn.style.display = jugador.inventario.orbe > 0 ? "block" : "none";
+    equiparArmaBtn.style.display = jugador.inventario.espada > 0 ? "block" : "none";
     equiparEspadaLegendariaBtn.style.display = jugador.inventario.espadaLegendaria > 0 ? "block" : "none";
+    equiparArmaduraBtn.style.display = jugador.inventario.armadura > 0 ? "block" : "none";
     equiparArmaduraEpicaBtn.style.display = jugador.inventario.armaduraEpica > 0 ? "block" : "none";
 
     // 🔹 Botón de inventario siempre visible
